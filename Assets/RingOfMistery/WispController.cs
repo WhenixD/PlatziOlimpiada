8f1d00f563abaead1bb79b339cda3f1bc4401af2
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2021-03-03 18:10:08 -0500

    First commit

---
+++ Assets/RingOfMistery/WispController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WispController : MonoBehaviour {
+    Vector3 mousePosition;
+    public float velocity;
+
+    void Update() {
+        mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0f);
+        transform.position = Vector3.Lerp(transform.position, mousePosition, velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour {
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeGrounded = .1f;
+    float accelerationTimeAirborne = .2f;
+    public float moveSpeed = 6;
+
+    public float gravity;
+    public float maxJumpVelocity;
+    public float minJumpVelocity;
+
+    bool jumpRequest;
+
+    Vector3 velocity;
+
+    float velocityXSmoothing;
+
+    Controller2D controller;
+
+    private SpriteRenderer sprite;
+
+    public GameObject cameraAux;
+
+    void Start() {
+        controller = GetComponent<Controller2D>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpVelocity);
+
+        sprite = GetComponent<SpriteRenderer>();
+
+        jumpRequest = false;
+    }
+
+    void Update() {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        int wallDirX = (controller.collisions.left) ? -1 : 1;
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing,
+        (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+        if (controller.collisions.above || controller.collisions.below) {
+            velocity.y = 0;
+
+            if (jumpRequest) {
+                jumpRequest = false;
+            }
+        }
+
+        if (velocity.y < 0) {
+            jumpRequest = true;
+        }
+
+        if (Input.GetButtonDown("Jump") && (controller.collisions.above || controller.collisions.below)) {
+            jumpRequest = true;
+            velocity.y = maxJumpVelocity;
+        }
+
+        if (Input.GetButtonUp("Jump") && velocity.y > minJumpVelocity) {
+            velocity.y = minJumpVelocity;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime, input);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        string otherTag = other.tag.ToString();
+
+        switch (otherTag) {
+            case "Collectable":
+                break;
+
+            case "Hazard":
+                DestroyPlayer();
+                break;
+
+            case "Limit":
+                DestroyPlayer();
+                break;
+        }
+    }
+
+    void DestroyPlayer() {
+        cameraAux.GetComponent<CameraFollow>().enabled = false;
+        Destroy(gameObject);
+        //FindObjectOfType<GameManager>().ResetLevel();
+    }
+}
+
