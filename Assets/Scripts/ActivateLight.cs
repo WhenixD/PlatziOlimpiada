bc2029ba8cf185a358af90d89266a0ba51e11297
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2021-03-05 10:23:59 -0500

    Added menu

--- Assets/Scripts/ActivateLight.cs
+++ Assets/Scripts/ActivateLight.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 public class ActivateLight : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
-
-        if (other.tag == "Player") {
-            GetComponent<ActivateLight>().enabled = true;
-        }
-
+        GetComponentInParent<WispController>().enabled = true;
     }
 }
